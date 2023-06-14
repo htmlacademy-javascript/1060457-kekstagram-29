0be5d55b3d7eefd4f47e2bf88800d04238e82a03
Маргарита Телкова <n5212549@ya.ru>
2023-06-14 22:15:57 +0300

    fixed the problem

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,13 @@
 // Функция для проверки длины строки.
 
-let checkLength = (word, length) => {
-  return word.length <= length;
-}
+const checkLength = (word, length) => word.length <= length;
 
 // console.log(checkLength('дерево', 4));
 
 
 // Функция для проверки, является ли строка палиндромом.
 
-let checkPalindrom = (word) => {
+const checkPalindrom = (word) => {
   const newWord = word.replaceAll(' ', '').toLowerCase();
   for (let i = 0; i < newWord.length / 2; i++) {
     if (newWord[i] !== newWord[newWord.length - i - 1]) {
