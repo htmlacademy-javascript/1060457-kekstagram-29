3c3acad5baabb2bcd4d0c0bc3234e73e1167f03b
Keks <keks@htmlacademy.ru>
2023-06-14 22:19:49 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,21 @@
+// Функция для проверки длины строки.
+
+const checkLength = (word, length) => word.length <= length;
+
+(checkLength('дерево', 4));
+
+
+// Функция для проверки, является ли строка палиндромом.
+
+const checkPalindrom = (word) => {
+  const newWord = word.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < newWord.length / 2; i++) {
+    if (newWord[i] !== newWord[newWord.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+(checkPalindrom('Сел в озере березов лес'));
+
