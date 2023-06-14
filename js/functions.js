11f7cf128989dee35c61584a88669eb4723c3e7a
Маргарита Телкова <n5212549@ya.ru>
2023-06-14 22:17:36 +0300

    again fixed the problem

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@
 
 const checkLength = (word, length) => word.length <= length;
 
-// console.log(checkLength('дерево', 4));
+(checkLength('дерево', 4));
 
 
 // Функция для проверки, является ли строка палиндромом.
@@ -17,5 +17,5 @@ const checkPalindrom = (word) => {
   return true;
 };
 
-// console.log(checkPalindrom('Сел в озере березов лес'));
+(checkPalindrom('Сел в озере березов лес'));
 
