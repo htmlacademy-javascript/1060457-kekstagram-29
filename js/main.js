646fd9b554da049fc5673c6cc4ba9ff2e0931903
Маргарита Телкова <n5212549@ya.ru>
2023-07-28 00:20:04 +0300

    complete two tasks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import './full-sizeImage.js';
+import './form.js';
 import {similarPhotos} from './data.js';
 
-const pictures = similarPhotos(6);
+const pictures = similarPhotos(25);
 
 import {renderMiniPic} from './creatingMiniatures.js';
 
