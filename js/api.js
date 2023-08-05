89b1b827fd9b9e656e91d9d9869b22d80dc939fe
Rita <115425262+succubbb@users.noreply.github.com>
2023-08-05 22:25:49 +0300

    Revert "final pull-request"

--- js/api.js
+++ js/api.js
@@ -5,11 +5,11 @@ const Route = {
   SEND_DATA: '/',
 };
 
-const ERROR_GET = 'Ошибка загрузки данных';
+const ERROR_GET = 'Не удаётся загрузить данные. Проверьте подключение к сети !';
 const ERROR_SEND = 'Возникла ошибка при отправке';
 
 const load = (route, errorText, method = 'GET', body = null) =>
-  fetch(`${SERVER}${route}`, { method, body })
+  fetch(`${SERVER}${route}`, {method, body})
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -20,6 +20,7 @@ const load = (route, errorText, method = 'GET', body = null) =>
       throw new Error(errorText);
     });
 
-export const getData = () => load(Route.GET_DATA, ERROR_GET, 'GET');
-export const sendData = (body) => load(Route.SEND_DATA, ERROR_SEND, 'POST', body);
+const getData = () => load(Route.GET_DATA, ERROR_GET, 'GET');
+const sendData = (body) => load(Route.SEND_DATA, ERROR_SEND, 'POST', body);
 
+export {getData, sendData};
