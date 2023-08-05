c6a5e89de58e125086a31d11c10a4e1a9befbfc7
Маргарита Телкова <n5212549@ya.ru>
2023-08-05 17:45:26 +0300

    add import

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { getData } from './api.js';
 import { showAlert } from './util.js';
 import { renderMiniPic } from './creating-miniatures.js';
 import { filterGallery } from './filters.js';
+import './add-new-pic.js';
 
 
 getData()
